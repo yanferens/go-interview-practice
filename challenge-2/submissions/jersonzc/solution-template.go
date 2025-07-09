ef10e1e891e427fafff14206dda37d01d40c2406
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-07-09 13:39:45 -0500

    Add solutions for challenge 1 and 2 by  jersonzc (#58)
    * feat: add solution for challenge 1
    
    * feat: add solution for challenge 2

---
+++ challenge-2/submissions/jersonzc/solution-template.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	var result strings.Builder
+	for i := len(s) - 1; i >= 0; i-- {
+		result.WriteByte(s[i])
+	}
+	return result.String()
+}
