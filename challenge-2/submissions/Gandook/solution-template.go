882a2a4f1245fa0faf9db63e7784fbb4cdea7b47
Mehrad Zamani <33395604+Gandook@users.noreply.github.com>
2025-07-13 02:22:34 +0200

    Add solution for Challenges by Gandook (#72)
    * Add solution for Challenge 1 by Gandook
    
    * Add solution for Challenge 2 by Gandook
    
    * Add solution for Challenge 3 by Gandook

---
+++ challenge-2/submissions/Gandook/solution-template.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	runes := []rune(s)
+	result := make([]rune, len(s))
+	for i, r := range runes {
+	    result[len(s) - i - 1] = r
+	}
+	return string(result)
+}
