b63c2365e102f01a1e046d699f14c081c0a1e987
ingingx <yingpengma@gmail.com>
2025-07-21 19:22:43 +0200

    Add solution for Challenges by ingingX (#122)
    * Add solution for Challenge 1 by ingingX
    
    * Add solution for Challenge 2 by ingingX
    
    * Add solution for Challenge 3 by ingingX

---
+++ challenge-2/submissions/ingingX/solution-template.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	// TODO: Implement the function
+	runeSlice := []rune(s)
+	for i, j := 0, len(runeSlice)-1; i < j; i, j = i+1, j-1 {
+		runeSlice[i], runeSlice[j] = runeSlice[j], runeSlice[i] // Swap
+	}
+	return string(runeSlice)
+}
