1e15706570a72361043027e3ee096eefe40eac77
Polina <88816018+PolinaSvet@users.noreply.github.com>
2025-07-22 20:38:10 +0400

    Add solution for Challenges by PolinaSvet (#136)
    * Add solution for Challenge 21 by PolinaSvet
    
    * Add solution for Challenge 22 by PolinaSvet

---
+++ challenge-22/submissions/PolinaSvet/solution-template.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	// Standard U.S. coin denominations in cents
+	denominations := []int{1, 5, 10, 25, 50}
+
+	// Test amounts
+	amounts := []int{87, 42, 99, 33, 7}
+
+	for _, amount := range amounts {
+		// Find minimum number of coins
+		minCoins := MinCoins(amount, denominations)
+
+		// Find coin combination
+		coinCombo := CoinCombination(amount, denominations)
+
+		// Print results
+		fmt.Printf("Amount: %d cents\n", amount)
+		fmt.Printf("Minimum coins needed: %d\n", minCoins)
+		fmt.Printf("Coin combination: %v\n", coinCombo)
+		fmt.Println("---------------------------")
+	}
+}
+
+// MinCoins returns the minimum number of coins needed to make the given amount.
+// If the amount cannot be made with the given denominations, return -1.
+func MinCoins(amount int, denominations []int) int {
+	// TODO: Implement this function
+	if amount == 0 {
+		return 0
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(denominations)))
+	x := len(denominations)
+	cnt := 0
+	am := amount
+	for x > 0 {
+		i := len(denominations) - x
+		coin := denominations[i]
+
+		for {
+			if am-coin >= 0 {
+				cnt++
+				am -= coin
+			} else {
+				x--
+				break
+			}
+		}
+	}
+
+	if cnt == 0 {
+		return -1
+	}
+	return cnt
+}
+
+// CoinCombination returns a map with the specific combination of coins that gives
+// the minimum number. The keys are coin denominations and values are the number of
+// coins used for each denomination.
+// If the amount cannot be made with the given denominations, return an empty map.
+func CoinCombination(amount int, denominations []int) map[int]int {
+	sort.Sort(sort.Reverse(sort.IntSlice(denominations)))
+	x := len(denominations)
+	cnt := 0
+	am := amount
+	m := make(map[int]int)
+	for x > 0 {
+		i := len(denominations) - x
+		coin := denominations[i]
+
+		for {
+			if am-coin >= 0 {
+				cnt++
+				am -= coin
+				m[coin]++
+			} else {
+				x--
+				break
+			}
+		}
+
+	}
+	return m
+}
