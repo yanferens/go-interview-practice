aeed9d57ca21a606237f983d72a11386b91400dd
perekoshik <153132803+perekoshik@users.noreply.github.com>
2025-07-30 20:18:53 +0300

    Add solution for Challenges by perekoshik (#169)
    * Add solution for Challenge 1 by perekoshik
    
    * Add recursion solution for Challenge 2 by perekoshik

---
+++ challenge-2/submissions/perekoshik/solution-template.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+    if len(s) == 0{
+        return ""
+    }
+    return ReverseString(s[1:]) + string(s[0])
+}
