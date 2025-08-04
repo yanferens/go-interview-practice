b80a35a51e2f47c60d746b72e51751d7df57790b
Mohammad Hossein Naghinezhad <114654696+naghinezhad@users.noreply.github.com>
2025-08-05 00:17:08 +0330

    Add solution for Challenges by naghinezhad (#201)
    * Add solution for Challenge 1 by naghinezhad
    
    * Add solution for Challenge 2 by naghinezhad

---
+++ challenge-2/submissions/naghinezhad/solution-template.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	if len([]rune(s)) > 1000 {
+		return ""
+	}
+	
+	runes := []rune(s)
+	
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	
+	return string(runes)
+}
