d976fcdb117665e700dc0b52f43238d9c482716b
omid9h <67994190+omid9h@users.noreply.github.com>
2025-08-08 19:01:26 +0330

    Add solution for Challenge 2 by omid9h (#224)

---
+++ challenge-2/submissions/omid9h/solution-template.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+    var result []byte
+	for i:=len(s)-1; i>=0; i-- {
+	    result = append(result, s[i])
+	}
+	return string(result)
+}
