a45604b4530e86845553c4324d4289282dcfd763
Mersad <nickmersad81@gmail.com>
2025-08-11 22:33:31 +0330

    Add solution for Challenges by Mersad-Moghaddam (#227)
    * Add solution for Challenge 1 by Mersad-Moghaddam
    
    * Add solution for Challenge 2 by Mersad-Moghaddam

---
+++ challenge-2/submissions/Mersad-Moghaddam/solution-template.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	// TODO: Implement the function
+	runes := []rune(s)
+	n := len(runes)
+
+	// Swap characters from both ends
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-1-i] = runes[n-1-i], runes[i]
+	}
+
+	return string(runes)
+}
